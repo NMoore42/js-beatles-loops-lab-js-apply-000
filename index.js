58435c54660dada6742ae56a82f8054515c9bbcd
organized-quotation-1910 <nicholas.moore5@icloud.com>
2019-01-14 21:49:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // add solution here
-const = [];
+let beatlesMembers = [];
 
 function theBeatlesPlay(musicians, instruments){
-  
+  for (i=0; i<musicians.length; i++){
+    beatlesMembers[i] = `$(musicians[i]) plays $(instruments[i])`
+  }
+  return beatlesMembers;
 }
