5077b9b1346f23e93410a25cc12995840fe20be6
organized-quotation-1910 <nicholas.moore5@icloud.com>
2019-01-14 22:02:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts(facts){
   let i = 0;
-  while (facts[i].endsWith("!!!") !==true) {
+  while (facts.endsWith("!!!") !==true) {
     facts[i]+= "!!!";
     i++;
   }
