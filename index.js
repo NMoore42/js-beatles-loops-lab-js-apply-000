e825ec521a5da89596be7a64638b81d077912a9b
organized-quotation-1910 <nicholas.moore5@icloud.com>
2019-01-14 22:42:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function johnLennonFacts(facts){
 function iLoveTheBeatles(number){
   let iLoveArray = [];
   do {
-    iLoveArray.pop("I love the Beatles!") ;
+    iLoveArray.push("I love the Beatles!") ;
   } while (iLoveArray.length <= number)
 }
